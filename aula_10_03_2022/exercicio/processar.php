b1a33200acb64c59937e972877f02d12e694fd52
Daiellys <80227938+daiellys64@users.noreply.github.com>
2022-03-10 09:10:17 -0300

    Add files via upload

---
+++ aula_10_03_2022/exercicio/processar.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$num3 = $_POST['num3'];
+
+$soma= $num1 + $num2 + $num3;
+
+echo "A soma de ".$num1." + ". $num2."." + ". $num2." é igual a ".$soma ;
+
+?>
