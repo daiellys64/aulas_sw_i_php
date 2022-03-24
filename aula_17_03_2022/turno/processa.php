2671d48a1977eac46a34f7df3fb6042d3767947f
Daiellys <80227938+daiellys64@users.noreply.github.com>
2022-03-24 20:14:14 -0300

    Update processa.php

--- aula_17_03_2022/turno/processa.php
+++ aula_17_03_2022/turno/processa.php
@@ -14,16 +14,16 @@
 
         switch ($turno){
             case "M":
-                echo "<h3>Bom dia, $nome !</h3>";
+                echo "<h2>Bom dia, $nome !</h2>";
                 break;
             case "T":
-                echo "<h3>Boa Tarde, $nome !</h3>";
+                echo "<h2>Boa Tarde, $nome !</h2>";
                 break;
             case "N":
-                echo "<h3>Boa noite, $nome !</h3>";
+                echo "<h2>Boa noite, $nome !</h2>";
                 break;
             default:
-                echo "<h3>Carácter incorreto $nome , digite novamente.</h3>";
+                echo "<h2>Carácter incorreto $nome , digite novamente.</h2>";
                 break;
         }
     ?>
